6a7b9c4f590f790485caf30986c26238042379c1
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2024-03-20 11:37:57 +0530

    Linked Lists Solutions

---
+++ Linked Lists/Remove Duplicates from Sorted List.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#	def __init__(self, x):
+#		self.val = x
+#		self.next = None
+
+class Solution:
+	# @param A : head node of linked list
+	# @return the head node in the linked list
+	def deleteDuplicates(self, A):
+        current = A
+        while current is not None and current.next is not None:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return A

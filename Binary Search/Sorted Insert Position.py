bb019cff353b676a6c3e302aa682c8e5cd1510d9
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2024-03-20 19:03:30 +0530

    Binary Search Solutions

---
+++ Binary Search/Sorted Insert Position.py
@@ -0,0 +1,19 @@
+class Solution:
+    # @param A : list of integers
+    # @param B : integer
+    # @return an integer
+    def searchInsert(self, A, B):
+        low = 0
+        high = len(A) - 1
+        
+        while low <= high:
+            mid = (low + high) // 2
+            
+            if A[mid] == B:
+                return mid
+            elif A[mid] < B:
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return low

0413ff3ae52cbfc21be9cfe2899ff21f5ffa7cb3
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2024-03-23 00:38:46 +0530

    Linked Lists Solutions

---
+++ Linked Lists/Replace With Factors!.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#    def __init__(self, x):
+#        self.val = x
+#        self.next = None
+
+class Solution:
+    # @param A : head node of linked list
+    # @param B : integer
+    # @return the head node in the linked list
+    def solve(self, A, B):
+        current = A
+
+        while current is not None:
+            current.val = (current.val // B) * B
+            current = current.next
+
+        return A

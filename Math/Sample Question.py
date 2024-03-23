c3b964f127ab12620dc6842bff98887ddfbec80f
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2024-03-24 00:33:18 +0530

    Math Solutions

---
+++ Math/Sample Question.py
@@ -0,0 +1,8 @@
+class Solution:
+     # @param A : integer
+     # @param B : integer
+     # @return an integer
+     def solve(self, A, B):
+        modulo_value = 10**7
+        result = (A + B) % modulo_value
+        return result

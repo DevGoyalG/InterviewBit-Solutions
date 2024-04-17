be44e64f97c1a99b6537be660bb120b7cc72ae03
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2024-04-17 23:40:56 +0530

    Dynamic Programming Solutions

---
+++ Dynamic Programming/Distinct Subsequences.py
@@ -0,0 +1,20 @@
+class Solution:
+    # @param A : string
+    # @param B : string
+    # @return an integer
+    def numDistinct(self, A, B):
+        m, n = len(A), len(B)
+        
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        for i in range(m + 1):
+            dp[i][0] = 1
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if A[i - 1] == B[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j]
+        
+        return dp[m][n]
